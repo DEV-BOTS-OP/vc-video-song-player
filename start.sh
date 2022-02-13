1ec08e63dee6e03a8defc215a64df892034e8225
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-02-13 05:35:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/Devansh20055/vc-video-song-player /vc-video-song-player
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/Devansh20055/vc-video-song-player -b $BRANCH /vc-video-song-player
 fi
-cd /VCPlayerBot
+cd /vc-video-song-player
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
